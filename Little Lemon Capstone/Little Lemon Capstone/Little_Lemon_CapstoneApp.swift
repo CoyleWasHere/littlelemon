dac16def28e0020f572175a6aa8b76e0c727983a
CoyleWasHere <64401148+CoyleWasHere@users.noreply.github.com>
2023-07-11 19:29:55 -0400

    initial project

---
+++ Little Lemon Capstone/Little Lemon Capstone/Little_Lemon_CapstoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_Lemon_CapstoneApp.swift
+//  Little Lemon Capstone
+//
+//  Created by Daniel Coyle on 7/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Little_Lemon_CapstoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
